5bd9c6578f786fa5fbde4fde3566007270d4e818
yizhen.fan <yizhen.fan@baishancloud.com>
2018-10-10 16:12:50 +0800

    feat: 更新 igroot-fetch 接入 的代码

--- src/util/base.js
+++ src/util/base.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { notification } from 'igroot'
+import { notification, message } from 'igroot'
 import Fetch from 'igroot-fetch'
-
+import { handleTokenInvalid } from 'sso-login'
 import { getDomain } from '@/util/function'
 
 window.Fetch = Fetch
 window.React = React
-
-Fetch.setDomain(getDomain())
 
 /**
  * //需要自定义网络错误处理 请重写以下这个方法
@@ -20,32 +18,48 @@ Fetch.setDomain(getDomain())
  * handleGraphQLErrors: handleGraphQLErrors(errors, data)
  */
 
-// GraphQL 错误处理 (示例代码)
-function handleGraphQLErrors(errors, data) {
-  // message 错误信息 
-  // error_code 错误状态码 
-  // path 当前操作的graphql方法 
-  const { message, error_code, path } = errors[0]
+// 后端开发基于 bungalow 框架，使用这个错误处理的配置
+const errorConfig = {
+  handleGraphQLErrors: (errors, data) => {
+    // message 错误信息 
+    // error_code 错误状态码 
+    // path 当前操作的graphql方法 
+    const { message, error_code, path } = errors[0]
 
-  notification.error({
-    placement: 'bottomRight',
-    message: `操作【${path[0]}】失败`,
-    description: `错误信息:${message}`,
-  })
+    notification.error({
+      placement: 'bottomRight',
+      message: `操作【${path[0]}】失败`,
+      description: `错误信息:${message}`,
+    })
 
-  throw errors[0]
+    throw errors[0]
+  }
 }
-const error_config = {
-  handleGraphQLErrors
+
+// 后端开发基于 Phalcon 框架，使用这个错误处理的配置
+const newErrorConfig = {
+  handleErrors: ({ code, msg }) => {
+    if (code == '44') {
+      handleTokenInvalid(getDomain())
+      return
+    }
+    if (code != 0) {
+      message.error(msg)
+    }
+  },
+  handleHttpErrors: (response) => {
+    notification.error({
+      message: 'Http Error',
+      description: response.statusText
+    })
+  }
 }
 
-window.Client = Fetch('/graphql', error_config)
+window.Client = Fetch(`${getDomain}/graphql`, newErrorConfig)
 
 /**
- * 如果您的系统需要对接多个后端,
- * 请打开以下注释并
- * 修改模块名的名称
+ * 如果您的系统后端有多个业务模块,
+ * 则按照以下操作
+ * 每一个模块可以新挂载一个请求对象
  */
-// window.ClientDemo2(Demo2 使用驼峰) = Fetch('模块名/graphql', {
-//   handleGraphQLErrors: this.aaa,
-// })
+// window.ClientDemo2 = Fetch(`${getDomain}/业务模块名/graphql`, newErrorConfig)
