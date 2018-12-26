fa343961eec29ac788a384691dbf845bcf3c9d8c
quanning.guo <463605498@qq.com>
2018-12-26 16:01:59 +0800

    feat: igroot 模板升级匹配最新的SL

--- src/pages/index/App.jsx
+++ src/pages/index/App.jsx
@@ -6,14 +6,17 @@ import RouterConfig from 'igroot-router-config'
 
 import { withLogin } from 'sso-login'
 import { getDomain } from '@/util/function'
+import { hot } from '#'
 const domain = getDomain()
 // 路由配置
 import config from './routerConfig'
 
 // 接入登录的时候取消下面这一行的注释
+@hot(module)
 // @withLogin(domain)
-export class App extends Component {
+class App extends Component {
   render() {
     return <RouterConfig config={config} />
   }
 }
+export default App
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -3,21 +3,22 @@ import './index.scss'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import { AppContainer } from '#'
 import '@/util/base.js'
 
 // App 
-import { App } from './App'
+import App from './App'
+import { LocaleProvider } from 'igroot'
+import zh_CN from 'igroot/lib/locale-provider/zh_CN'
 
 const render = App => ReactDOM.render(
-  <AppContainer>
+  <LocaleProvider locale={zh_CN}>
     <App />
-  </AppContainer>,
+  </LocaleProvider>,
   document.getElementById('igroot-container')
 )
 
 render(App)
 
 if (module.hot) {
-  module.hot.accept('./App', () => render(App))
+  module.hot.accept('./App')
 }
